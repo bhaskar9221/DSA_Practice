12b897e9127791fc9c3b6838479b3e5d78f59e10
Bhaskar <75377209+bhaskar9221@users.noreply.github.com>
2026-04-13 14:29:58 +0530

    Time: 35 ms (0.00%) | Memory: 19.4 MB (100.00%) - LeetSync

---
+++ Array/4280-count-digit-appearances/count-digit-appearances.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countDigitOccurrences(self, nums: list[int], digit: int) -> int:
+        count = 0
+        for num in nums:
+            while num>0:
+                if num%10 == digit:
+                    count += 1
+                num //= 10
+        return count
