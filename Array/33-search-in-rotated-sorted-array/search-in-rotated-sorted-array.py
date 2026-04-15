f6df611397c5a316c145f263e1cde602279ea4cc
Bhaskar <75377209+bhaskar9221@users.noreply.github.com>
2026-04-16 00:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.89%) - LeetSync

--- Array/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ Array/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,63 +1,22 @@
 class Solution:
-
     def search(self, nums: List[int], target: int) -> int:
+        i,j = 0, len(nums)-1
 
-        n = len(nums)
+        while i<=j:
+            mid = (i+j)//2
 
-        l = 0
+            if nums[mid] == target:
+                return mid
 
-        r = n - 1
+            if nums[i] <= nums[mid]:
+                if nums[i] <= target < nums[mid]:
+                    j = mid - 1
+                else:
+                    i = mid + 1
 
-
-
-        while l < r:
-
-            m = (l + r) // 2
-
-
-
-            if nums[m] > nums[r]:
-
-                l = m + 1
-
-            else:
-
-                r = m
-
-
-
-        min_i = l
-
-
-
-        if min_i == 0:
-
-            l, r = 0, n - 1
-
-        elif target >= nums[0] and target <= nums[min_i - 1]:
-
-            l, r = 0, min_i - 1
-
-        else:
-
-            l, r = min_i, n - 1
-
-
-
-        while l <= r:
-
-            m = (l + r) // 2
-
-            if nums[m] == target:
-
-                return m
-
-            elif nums[m] < target:
-
-                l = m + 1
-
-            else:
-
-                r = m - 1
-
+            else :
+                if nums[mid] < target <= nums[j]:
+                    i = mid + 1
+                else:
+                    j = mid - 1
         return -1
