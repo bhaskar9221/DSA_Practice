d1f63afe43f74940c6f9f162dd32d25286a9765a
Bhaskar <75377209+bhaskar9221@users.noreply.github.com>
2026-08-05 13:10:59 +0530

    Time: 765 ms (9.09%) | Memory: 153.5 MB (16.67%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,37 @@
+class Solution:
+    def remainingMethods(
+        self, n: int, k: int, invocations: List[List[int]]
+    ) -> List[int]:
+        directed_graph = [[] for _ in range(n)]
+        undirected_graph = [[] for _ in range(n)]
+      
+        for caller, callee in invocations:
+            directed_graph[caller].append(callee)
+            undirected_graph[caller].append(callee)
+            undirected_graph[callee].append(caller)
+      
+        is_suspicious = [False] * n
+      
+        def mark_suspicious_methods(method_id: int) -> None:
+            is_suspicious[method_id] = True
+            for next_method in directed_graph[method_id]:
+                if not is_suspicious[next_method]:
+                    mark_suspicious_methods(next_method)
+      
+        mark_suspicious_methods(k)
+      
+        visited = [False] * n
+      
+        def unmark_connected_suspicious(method_id: int) -> None:
+            visited[method_id] = True
+            for connected_method in undirected_graph[method_id]:
+                if not visited[connected_method]:
+                    is_suspicious[connected_method] = False
+                    unmark_connected_suspicious(connected_method)
+      
+       
+        for method_id in range(n):
+            if not is_suspicious[method_id] and not visited[method_id]:
+                unmark_connected_suspicious(method_id)
+      
+        return [method_id for method_id in range(n) if not is_suspicious[method_id]]
